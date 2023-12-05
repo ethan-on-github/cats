915937fe7b8b8b15e8fe30059fe29e13d8ea9abb
icemoss <138513719+ethan-on-github@users.noreply.github.com>
2023-12-05 15:59:46 +0000

    Fixes

--- sodium-static/public/dyn/dyn.config.js
+++ sodium-static/public/dyn/dyn.config.js
@@ -5,7 +5,7 @@ self.__dynamic$config = {
   logLevel: 3,
   bare: {
     version: 3,
-    path: "https://gobare.rubynetwork.tech/"
+    path: "https://phantomnetwork.cloud/bare/"
   },
   tab: {
     title: 'Service',
